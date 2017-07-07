be230c1f085846aba05c5600466934b5e6df2ef7
suadesh <m.dadamo@me.com>
2017-07-07 18:10:23 +0200

    feat: poi_id.py modififed to perform test
    The code now is functional to perform a test, predict , and calculate score ,  recall and precision score .
    Question file added , but not compiled.
    License modified.
    
    Need to work on outliers.

--- poi_id.py
+++ poi_id.py
@@ -2,21 +2,59 @@
 
 import sys
 import pickle
+import matplotlib.pyplot
+
 sys.path.append("../tools/")
+sys.path.append(
+        "/Users/massi/Google_Drive/Perso/Data_Science/Udacity/ud120-projects-master/tools/")
+sys.path.append(
+        "/Users/massi/Google_Drive/Perso/Data_Science/Udacity/ud120-projects-master/Enron_Email_Dataset_investing/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary','to_messages','deferral_payments','total_payments',
+                 'exercised_stock_options','bonus','restricted_stock','shared_receipt_with_poi', 
+                 'restricted_stock_deferred','total_stock_value','expenses','loan_advances',
+                 'from_messages','other','from_this_person_to_poi','director_fees',
+                 'deferred_income','long_term_incentive','email_address', 
+                 'from_poi_to_this_person'] 
+                    # You will need to use more features
 
 ### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
+with open(
+        "/Users/massi/Google_Drive/Perso/Data_Science/Udacity/ud120-projects-master/Enron_Email_Dataset_investing/final_project_dataset.pkl", 
+        "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+print "the data set is composed of ", len(data_dict), " elemets" 
+
+
+
+### Total is removed becouse is not only an outlier but also not a prerson,
+### is the Total.. 
+print data_dict['TOTAL']
+data_dict.pop('TOTAL')
+
+
+''' LISTA DI TUTTE LE FEATURES DISPONIBILI 
+'salary','to_messages','deferral_payments','total_payments',
+'exercised_stock_options','bonus','restricted_stock','shared_receipt_with_poi', 
+'restricted_stock_deferred','total_stock_value','expenses','loan_advances',
+'from_messages','other','from_this_person_to_poi','director_fees',
+'deferred_income',  'long_term_incentive', 'email_address', 
+'from_poi_to_this_person'
+'''
+
+## BEST POI SALARY BONUS EXERCISED-STOCK TOTAL-STOCK EXPENSES FROM_THIS_PERSON FROM_POI_TO OTHER(FORSE)  
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -24,6 +62,29 @@ my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+i = 1 
+
+for i in range(7):
+    for point in data:
+        AA = point[i]
+        BB = point[i+1]
+        matplotlib.pyplot.scatter( AA, BB )
+
+    matplotlib.pyplot.xlabel(features_list[i])
+    matplotlib.pyplot.ylabel(features_list[i+1])
+    matplotlib.pyplot.show()
+    i +=1 
+
+for point in data:
+    salary = point[1]
+    exercised_stock_options = point[2]
+    matplotlib.pyplot.scatter( salary, exercised_stock_options )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("exercised_stock_options")
+matplotlib.pyplot.show()
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -35,18 +96,40 @@ labels, features = targetFeatureSplit(data)
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
+## SO FAR THE BEST ONE IS NAIVE BAYES 
+
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
 ### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+### http://scikit-lcearn.org/stable/modules/generated/sklearn.cross_validation.
+### StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(clf,features_test , labels_test)
+print scores.mean() 
+
+print clf.score(features_test,labels_test)
+
+
+print precision_score(labels_test,pred)
+print recall_score(labels_test,pred)
+
+o = 0 
+for i in pred: 
+    print i , labels_test[o]
+    o+=1
+
+    
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
